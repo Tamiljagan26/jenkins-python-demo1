c00e2129bcd4377b7ce7ee2f064e5685ae1616da
Tamiljagan26 <tamiljagan26@gmail.com>
2026-02-24 15:27:50 +0530

    removed interactive input for jenkins

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ def guess_number():
     number = 5
     guess = 5   # hardcoded for CI
     if guess == number:
-        print("Correct Guess 🔥")
+        print("Correct Guess")
     else:
         print("Wrong Guess")
 
